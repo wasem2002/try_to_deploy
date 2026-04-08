2b2bf42099f51df4696c88adc1931a4388a55669
wasem2002 <taltatalta41@gmail.com>
2026-04-09 01:19:06 +0200

    Update settings.py

--- calculator/settings.py
+++ calculator/settings.py
@@ -105,7 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+# settings.py - add this
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
