a388bdddb9941ffecdad8216c5dfe3d7a850647a
wasem2002 <taltatalta41@gmail.com>
2026-04-09 01:22:03 +0200

    Simplify static file collection in build script
    Removed redundant staticfiles directory creation and listing.

--- build.sh
+++ build.sh
@@ -3,9 +3,4 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Ensure staticfiles directory exists and is in the right place
-mkdir -p staticfiles
-python manage.py collectstatic --no-input --clear
-
-# List what was collected for debugging
-ls -la staticfiles/
+python manage.py collectstatic --no-input
